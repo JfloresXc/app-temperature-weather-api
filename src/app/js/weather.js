f035817f75d2bd4bbdf93bc14c38f231deb5af02
unknown <jfloresxc@gmail.com>
2021-01-28 20:12:16 -0500

    Correción en las unidades de temperatura

--- src/app/js/weather.js
+++ src/app/js/weather.js
@@ -7,7 +7,7 @@ class Weather{
 
     async getData(){
         //Important : https://
-        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}`
+        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}&units=metric`
         const response = await fetch(URI)
         const data = await response.json()
         console.log(data)
